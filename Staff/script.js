4a234ad3a961525d89a1dad09f2ffaf9f57431f9
joshua <jaykayudo@gmail.com>
2022-04-05 13:44:11 +0100

    like fourth push

--- Staff/script.js
+++ Staff/script.js
@@ -6,3 +6,12 @@ window.onload = function () {
 });
 // document.querySelector('smart-calendar').view = 'landscape';
 }
+var logoutModal = document.getElementById('logout-modal');
+    var logoutModalClose = document.getElementById('logout-close');
+    logoutModalClose.addEventListener('click',function(){
+        logoutModal.classList.add('hidden');
+    });
+    var logoutModalOpen = document.getElementById('logout-open');
+    logoutModalOpen.addEventListener('click',function(){
+        logoutModal.classList.remove('hidden');
+    });
